88976122213a6f5a8ed3cb0bf32ede55dcaf2a41
devinsg <duyanh2005@gmail.com>
2019-04-07 13:53:56 +0000

    tweak page-2 => AboutUs

---
+++ src/pages/aboutus.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const AboutUs = () => (
+  <div>
+    <h1>Hi from the About Us</h1>
+    <p>About Us</p>
+    <Link to="/">Go back to the home page</Link>
+  </div>
+)
+
+export default AboutUs;
