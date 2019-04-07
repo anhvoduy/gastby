3453696a2f2d89d1397553b699898508ec0074b9
devinsg <duyanh2005@gmail.com>
2019-04-07 14:27:53 +0000

    define action.js

---
+++ src/action/action.js
@@ -0,0 +1,24 @@
+import api from '../api/api';
+
+const getUserList = async () => {
+    try {
+        return true;
+    }
+    catch (err) {
+        throw err;
+    }
+};
+
+const getProductList = async () => {
+    try {
+        return true;
+    }
+    catch (err) {
+        throw err;
+    }
+};
+
+export default {
+    getUserList,
+    getProductList
+};
