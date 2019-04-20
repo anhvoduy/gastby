766649e8530d0af4ffbc5633b396c5828b394575
Anh <anh.voduy@harveynash.vn>
2019-04-20 14:55:18 +0000

    FE: apply redux store & request AWS lambda function (#3)
    * apply redux store
    
    * init product page
    
    * tweak page-2 => AboutUs
    
    * wrap api
    
    * define Footer
    
    * tweak title name
    
    * tweak components
    
    * define action.js
    
    * update render: product name + image
    
    * clean up

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
 import Header from '../components/header'
+import Footer from '../components/footer'
 import './index.css'
 
 const Layout = ({ children, data }) => (
@@ -25,6 +26,7 @@ const Layout = ({ children, data }) => (
     >
       {children()}
     </div>
+    <Footer />
   </div>
 )
 
